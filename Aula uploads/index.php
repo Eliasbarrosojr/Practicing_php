4175774c8385fcd543fce476fb397d903d50e0ed
Elias Barroso Jr <eliasbarroso222@gmail.com>
2024-04-24 11:04:43 -0300

    feat:Uploading files with php

---
+++ Aula uploads/index.php
@@ -0,0 +1,27 @@
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name="fileUpload">
+    <Button type="submit">Enviar</Button>
+</form>
+
+<?php
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+    $file = $_FILES["fileUpload"];
+
+    if($file["error"]){
+        throw new Exception("Error:" .$file["error"]);
+    }
+
+    $dirUploads = "uploads";
+
+    if(!is_dir($dirUploads)) {
+        mkdir($dirUploads);
+    }
+
+    if(move_uploaded_file($file["tmp_name"], $dirUploads . DIRECTORY_SEPARATOR . $file["name"])){
+        echo "Upload realizado com sucesso!";
+    } else {
+        throw new Exception("Não foi possível realizar o upload.");
+    }
+}
+?>
